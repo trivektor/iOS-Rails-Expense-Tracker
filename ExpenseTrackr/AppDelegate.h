55785dacc74b5ed776b016ead2651f02bd28c64f
trivektor <trivektor@gmail.com>
2012-10-12 20:59:23 -0700

    Initial Commit

---
+++ ExpenseTrackr/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ExpenseTrackr
+//
+//  Created by Tri Vuong on 10/12/12.
+//  Copyright (c) 2012 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ExpenseTrackr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExpenseTrackr
+//
+//  Created by Tri Vuong on 10/12/12.
+//  Copyright (c) 2012 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
