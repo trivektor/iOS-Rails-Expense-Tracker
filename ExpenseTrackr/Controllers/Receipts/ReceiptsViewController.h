b1e2ba5f0470df89e4651a119790d87fdbfd65ef
trivektor <trivektor@gmail.com>
2012-11-10 16:33:32 -0800

    add empty ReceiptsViewController

---
+++ ExpenseTrackr/Controllers/Receipts/ReceiptsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReceiptsViewController.h
+//  ExpenseTrackr
+//
+//  Created by Tri Vuong on 11/9/12.
+//  Copyright (c) 2012 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceiptsViewController : UIViewController
+
+@end
---
+++ ExpenseTrackr/Controllers/Receipts/ReceiptsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ReceiptsViewController.m
+//  ExpenseTrackr
+//
+//  Created by Tri Vuong on 11/9/12.
+//  Copyright (c) 2012 Crafted By Tri. All rights reserved.
+//
+
+#import "ReceiptsViewController.h"
+
+@interface ReceiptsViewController ()
+
+@end
+
+@implementation ReceiptsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
